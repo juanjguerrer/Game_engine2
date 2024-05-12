8af6e12b6054a41c973e1a3a18a13214fe409561
Juan Guerrero <jjguerrerog02@gmail.com>
2024-05-12 14:04:20 -0600

    1.0 Display

---
+++ src/main/java/com/engineTester/Main.java
@@ -0,0 +1,10 @@
+package com.engineTester;
+
+import renderEngine.DisplayManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+    }
+}
